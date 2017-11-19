5d15d3d0f26f9e6eba988a7037cd61f4274e6802
mustafa220 <musto_220@windowslive.com>
2017-11-19 23:38:47 +0300

    routes and auth

--- App/Config/Route.php
+++ App/Config/Route.php
@@ -3,10 +3,25 @@
 		"directs" => [
 			"/" => "App\Controllers\HomeController@index",
 			"login.html" => "App\Controllers\IOController@login",
-			"newProduct.html" => "App\Controllers\TestController@newProduct",
-			"newProductAjax" => "App\Controllers\TestController@newProductAjax"
+			"logout.html" => "App\Controllers\IOController@logout",
+			"user.html" => "App\Controllers\UserController@index",
+			"singUp.html" => "App\Controllers\UserController@signUp",
+			"profile.html" => "App\Controllers\UserController@profile",
+			"editprofile.html" => "App\Controllers\UserController@editprofile",
+			"cart.html" => "App\Controllers\CartController@index",
+			"addcart.html" => "App\Controllers\CartController@addCart",
+			"newproduct.html" => "App\Controllers\ProductController@newproduct"
+			"tnewProduct.html" => "App\Controllers\TestController@newProduct",
+			"tnewProductAjax" => "App\Controllers\TestController@newProductAjax"
 		],
 		"preg_matches" =>[
+			"product/(.*?).html" => "App\Controllers\ProductController@product -> {productSef => [1]}",
+			"editproduct/(.*?).html" => "App\Controllers\ProductController@editproduct -> {productSef => [1]}",
+			"deleteproduct/(.*?).html" => "App\Controllers\ProductController@deleteproduct -> {productSef => [1]}",
+			"deletecart/(.*?).html" => "App\Controllers\CartController@deletecart -> { productId => [1] }",
+			"categories.html" => "App\Controllers\CategoryController@index",
+			"categories/(.*?).html" => "App\Controllers\CategoryController@category -> { categorySef => {1} }",
+			"ajaxSearch" => "App\Controllers\AjaxController@search"
 			//"/" => "App\Controllers\HomeController@index"
 			/*"Kategori/(.*?)/Sayfa/(.*?).html" => "App\Controllers\KategoriController@sayfa -> {kategoriSefLink => [1], sayfaId => [2]}",
 			"Kategori/(.*?).html" => "App\Controllers\KategoriController@index -> {kategoriSefLink => [1]}",
@@ -16,6 +31,7 @@
 			"cicekAdmin/kategoriSil/(.*?)" =>"App\Controllers\AdminController@kategoriSil -> {kategoriId => [1]}",
 			"cicekAdmin/cicekDuzenle/(.*?)" =>"App\Controllers\AdminController@cicekDuzenle2 -> {cicekId => [1]}",
 			"cicekAdmin/cicekSil/(.*?)" =>"App\Controllers\AdminController@cicekSil -> {cicekId => [1]}"*/
+			
 		],
 		"P404" => "App\Controllers\P404@index"
 	];
